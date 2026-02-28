b0a5aab01a097aeaefc9182b43afd128d3a6fad5
iandogless-creator <iandogless@gmail.com>
2026-02-28 10:46:03 +0000

    Remove deprecated GUI v2 subsystem
    - Fully delete legacy GUI v2 implementation
    - Migrate project to GUI v3 as sole interface
    - Remove obsolete adapters, models and controllers
    
    GUI v2 retired.

--- HVAC/gui_v2/common/__init__.py
+++
@@ -1,2 +0,0 @@
-# HVAC/gui_v2/common/__init__.py
-from .system_view_control import SystemViewControl
--- HVAC/gui_v2/modes/education/education_panel.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
-
-
-class EducationPanel(QWidget):
-    def __init__(self, view_state):
-        super().__init__()
-        layout = QVBoxLayout(self)
-        layout.addWidget(QLabel("Education Panel (v2 stub)"))
